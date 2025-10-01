3bf9b7201f8e15b3b521a49296b11fca8d42874f
𝒜𝓁𝒾𝒸𝑒 𝐿𝑒𝓂𝑜𝓈 🍭 <alice.lemos.costa@escola.pr.gov.br>
2025-10-01 15:57:58 -0300

    01/10

--- perguntas.js
+++ perguntas.js
@@ -2,17 +2,17 @@
 criaCartao(
     'Biologia',
     'O que é Genótipo?',
-    'é a constituição genética única de um indivíduo, representada pelos alelos (diferentes formas de um mesmo gene) herdados de cada um dos pais'
+    'é o conjunto de genes de um organismo.'
 )
 
 criaCartao(
     'Biologia',
     'O que é Fenótipo?',
-    'conjunto de características observáveis de um indivíduo, sejam elas físicas, fisiológicas, bioquímicas ou comportamentais, resultantes da interação entre o seu genótipo (sua composição genética) e fatores do ambiente'
+    'São as características físicas e comportamentais de um organismo.'
 )
 
 criaCartao(
     'Biologia',
     'Exemplos de Genótipo:',
-    'combinações de alelos, como AA, Aa e aa para um gene, que representam um organismo homozigoto dominante, heterozigoto e homozigoto recessivo, respetivamente'
+    'Cor dos olhos (BB, Bb, bb)Tipo sanguíneo (AA, AO, BB, BO) Sementes (SS, Ss, ss)'
 )
