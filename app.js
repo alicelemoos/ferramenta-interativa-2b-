20492116dd3950707ff6ce7ac2c516c0295ad98d
𝒜𝓁𝒾𝒸𝑒 𝐿𝑒𝓂𝑜𝓈 🍭 <alice.lemos.costa@escola.pr.gov.br>
2025-10-01 15:38:40 -0300

    01/10

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-fuction criaCartao (categoria, pergunta, resposta);
+fuction criaCartao (categoria, pergunta, resposta); {
+
 console.log(categoria, pergunta, resposta)
-}
 
 let container = document.getElementById('container')
 let cartao = document.createElement('article')
 cartao.className = 'cartao'
 
-cartao.innerHTML = `
-                <div class="cartao__conteudo">
-                    <h3>Programação</h3>
-                    <div class="cartao__conteudo__pergunta">
-                        <p>O que é CSS?</p>
-                    </div>
-                    <div class="cartao__conteudo__resposta">
-                        <p>O CSS é uma linguagem de estilização</p>
-                    </div>
-                </div>
+    //17-09-25//
+    cartao.innerHTML = `
+<div class="cartao__conteudo">
+
+    <h3>${categoria}</h3>
+    <div class="cartao__conteudo__pergunta">
+    <p>${pergunta}</p>
+    </div>
+    <div class="cartao__conteudo__resposta">
+    <p>${resposta}</p>
+    </div>
+</div>
+`
+    container.appendChild(cartão)
+
+}
